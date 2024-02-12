22fb7671ffb37b00527c6e8c54bd541948c95f55
Paritosh Maurya (TA Digital) <paritosh.maurya@tadigital.com>
2024-02-12 15:41:18 +0530

    Fixing build issues and adding postinstall command

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,7 +1,10 @@
 import BlogDetail from "@/components/blog-detail";
-import { getBlogBySlug, getLatestPublishedBlogs } from "../../../../actions/blogs";
+import {
+  getBlogBySlug,
+  getLatestPublishedBlogs,
+} from "../../../../actions/blogs";
 
-export default async function BlogDetailPage({params: {id}}) {
+export default async function BlogDetailPage({ params: { id } }) {
   const blog = await getBlogBySlug(id);
   return (
     <div>
@@ -12,19 +15,20 @@ export default async function BlogDetailPage({params: {id}}) {
 
 export async function generateStaticParams() {
   const blogs = await getLatestPublishedBlogs();
-  const data =  blogs?.slice(0,5)
-  if(data?.length) {
-    return data?.map(item => ({
-      id: item?.slug
-    }))
+  const data = blogs?.slice(0, 5);
+  if(!data.length) {
+    data.push({slug: 'Static page'});
   }
+  return data?.map((item) => ({
+    id: item?.slug,
+  }));
 }
 
-export async function generateMetadata({params: {id}}) {
+export async function generateMetadata({ params: { id } }) {
   const blog = await getBlogBySlug(id);
 
   return {
     title: blog?.title,
-    creator: blog?.userEmail
-  }
+    creator: blog?.userEmail,
+  };
 }
